19b538f81be94c85b572b41496c37ad56da1e624
StevenWuTG <stevenwu91@icloud.com>
2020-11-10 21:47:20 -0500

    users seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,93 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 puts "destroying seeds"
 Joke.destroy_all
 Gig.destroy_all
 Review.destroy_all
 User.destroy_all
 Club.destroy_all
+
+
+
+
+User.create!(user_name:Faker::Internet.username,
+        name:Faker::FunnyName.two_word_name ,
+        email:Faker::Internet.email,
+        pic:"https://kcrewsphoto.com/wp-content/uploads/2016/09/SantaBarbaraBabyModelHeadshotPhotography_05.jpg",
+        bio:Faker::Quote.famous_last_words ,
+        age:rand(18..70) ,
+        password:"password",
+        hometown:Faker::Address.full_address 
+         )
+
+
+
+         User.create!(user_name:Faker::Internet.username,
+         name:Faker::FunnyName.two_word_name ,
+         email:Faker::Internet.email,
+         pic:"https://cdn.onebauer.media/one/empire-tmdb/films/12110/images/y7N276seR43H31xvDbWMWxEsDr.jpg?quality=50&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg",
+         bio:Faker::Quote.famous_last_words ,
+         age:rand(18..70) ,
+         password:"password",
+         hometown:Faker::Address.full_address 
+          )
+
+
+          User.create!(user_name:Faker::Internet.username,
+          name:Faker::FunnyName.two_word_name ,
+          email:Faker::Internet.email,
+          pic:"https://www.sickchirpse.com/wp-content/uploads/2015/04/Angry-Old-Lady.jpg",
+          bio:Faker::Quote.famous_last_words ,
+          age:rand(18..70) ,
+          password:"password",
+          hometown:Faker::Address.full_address 
+           )
+
+        User.create!(user_name:Faker::Internet.username,
+        name:Faker::FunnyName.two_word_name ,
+        email:Faker::Internet.email,
+        pic:"https://images.freeimages.com/images/premium/previews/1212/12121329-beach-bro.jpg",
+        bio:Faker::Quote.famous_last_words ,
+        age:rand(18..70) ,
+        password:"password",
+        hometown:Faker::Address.full_address 
+         )
+
+
+         User.create!(user_name:Faker::Internet.username,
+        name:Faker::FunnyName.two_word_name ,
+        email:Faker::Internet.email,
+        pic:"https://media.proprofs.com/images/QM/user_images/2170919/1521925419.jpeg",
+        bio:Faker::Quote.famous_last_words ,
+        age:rand(18..70) ,
+        password:"password",
+        hometown:Faker::Address.full_address 
+         )
+
+
+
+         User.create!(user_name:Faker::Internet.username,
+        name:Faker::FunnyName.two_word_name ,
+        email:Faker::Internet.email,
+        pic:"https://i.guim.co.uk/img/media/73c104f8f62719499143da60768d67b4d71916e7/465_487_2374_1424/master/2374.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=43cf7615e777d12aa1ac42333e6e74b1",
+        bio:Faker::Quote.famous_last_words ,
+        age:rand(18..70) ,
+        password:"password",
+        hometown:Faker::Address.full_address 
+         )
+
+         User.create!(user_name:Faker::Internet.username,
+        name:Faker::FunnyName.two_word_name ,
+        email:Faker::Internet.email,
+        pic:"https://static01.nyt.com/images/2018/03/17/obituaries/17HAWKING/merlin_135590181_4b2e46cb-5d98-469c-8c01-792d9126d41c-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+        bio:Faker::Quote.famous_last_words ,
+        age:rand(18..70) ,
+        password:"password",
+        hometown:Faker::Address.full_address 
+         )
 
 
 puts "seeding users"
