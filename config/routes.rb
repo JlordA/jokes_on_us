448f8922f528b5ccdf3f6bc3d33f90ab2b76f299
StevenWuTG <stevenwu91@icloud.com>
2020-11-11 10:26:18 -0500

    changed homepage to user#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: "users#index"
   delete '/sessions/page_reset', to: 'sessions#page_reset', as: 'page_reset'
   delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
   get '/sessions/new', to: 'sessions#new', as: 'new_login'
